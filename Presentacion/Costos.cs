63e9c48125d1cf4a4f032381033e4d77d7ff07d6
luisDSotoE <ldsoto@unicesar.edu.co>
2022-11-08 23:05:23 -0500

    innit commit

--- Presentacion/Costos.cs
+++ Presentacion/Costos.cs
@@ -16,7 +16,7 @@ namespace Presentacion
         {
             InitializeComponent();
         }
-
+        //
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
